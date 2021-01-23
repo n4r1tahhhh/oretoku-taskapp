260f2c3ac59bdf4df7ce4f502fbbce4624f1553d
n4r1tahhhh <ryutaro5126@fuji.waseda.jp>
2021-01-23 09:36:21 +0000

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
+  before_save :downcase_email
+
+  # email
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  # password
   has_secure_password
+  validates :password, presence: true, allow_nil: true
+
+  private
+  def downcase_email
+    self.email.downcase!
+  end
 end
